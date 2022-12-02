77f2d8e5c89bde9f1c9f465470c0362962fe8b68
martynas.smicius@gmail.com <martynas.smicius@gmail.com>
2022-12-02 10:17:35 +0200

    Improved Admin site and added Type model

--- main/booking/admin.py
+++ main/booking/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from .models import Appliance, Booking
+from .models import Appliance, Booking, ApplianceType
 
-admin.site.register(Appliance)
-admin.site.register(Booking)
+class ApplianceInline(admin.TabularInline):
+    model = Appliance
+    fields = ('title', 'location', 'status',)
+    readonly_fields = ('title', 'location',)
+    can_delete = False
+    extra = 0
+
+class ApplianceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'location', 'type',  'status')
+    list_filter = ('type', 'status')
+    search_fields = ('id', 'title', 'location', 'type__type')
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('appliance', 'user', 'day_from', 'day_to')
+
+class ApplianceTypeAdmin(admin.ModelAdmin):
+    inlines = [ApplianceInline]
+
+admin.site.register(Appliance, ApplianceAdmin)
+admin.site.register(Booking, BookingAdmin)
+admin.site.register(ApplianceType, ApplianceTypeAdmin)
--- main/booking/models.py
+++ main/booking/models.py
@@ -1,17 +1,47 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Appliance(models.Model):
-    title = models.CharField(max_length=50)
-    location = models.CharField(max_length=50, default=None)
-    number = models.CharField(max_length=50)
-    description = models.CharField(max_length=1024, blank=True, null=True, default=None)
-    image_url = models.CharField(max_length=512, blank=True, null=True, default=None)
+class ApplianceType(models.Model):
+    type = models.CharField(max_length=50, help_text='Type of the appliance')
+
+    class Meta:
+        verbose_name = 'Type of Appliance'
+        verbose_name_plural = 'Types of Appliances'
 
     def __str__(self):
-        return f'{self.title}, {self.location}'
+        return f'{self.type}'
+
+class Appliance(models.Model):
+    """Model representing a specific appliance"""
+    title = models.CharField(max_length=50, help_text='Name\Title of the device')
+    location = models.CharField(max_length=50, default=None, help_text='Location of the device')
+    type = models.ForeignKey('ApplianceType', on_delete=models.SET_NULL, null=True)
+    number = models.CharField(max_length=50, help_text='Unique ID number, for engineering department')
+    description = models.CharField(max_length=1024, blank=True, null=True, default=None, help_text='Desciption of the device')
+    image_url = models.CharField(max_length=512, blank=True, null=True, default=None, help_text='Image URL for the device')
+
+    APPLIANCE_STATUS = (
+        ('a', 'Functional'),
+        ('b', 'Under repair')
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=APPLIANCE_STATUS,
+        blank=True,
+        default='a',
+        help_text='Status of the appliance'
+    )
+
+    def __str__(self):
+        return f'{self.title}, {self.location}, {self.type}'
+
+    class Meta:
+        verbose_name = 'Appliance'
+        verbose_name_plural = 'Appliances'
 
 class Booking(models.Model):
+    """Model representing a booking - appliance, time and user"""
     appliance = models.ForeignKey('Appliance', on_delete=models.SET_NULL, null=True)
     day_from = models.DateTimeField(null=True)
     day_to = models.DateTimeField(null=True)
@@ -19,3 +49,7 @@ class Booking(models.Model):
 
     def __str__(self):
         return f'{self.appliance.title}, {self.day_from} {self.day_to}'
+
+    class Meta:
+        verbose_name = 'Booking'
+        verbose_name_plural = 'Bookings'
