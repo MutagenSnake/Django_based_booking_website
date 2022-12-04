97a514aefd5bcf8d5606d2a40659243ee6f9de6a
martynas.smicius@gmail.com <martynas.smicius@gmail.com>
2022-12-04 15:07:40 +0200

    Reads equipment from API

--- main/booking/static/script/script.js
+++ main/booking/static/script/script.js
@@ -1,19 +1,29 @@
-//var dataSet = {{data|safe}};
-//    var dataSetLength = dataSet.length;
-//    const bookingElement = document.getElementById("bookingList");
-//    var bookings = [];
-//    for (var i = 0; i < dataSetLength; i++) {
-//        bookingHtml = `<li>${dataSet[i]}</li>`;
-//        bookings.push(bookingHtml);
-//        }
-//    bookingElement.innerHTML = bookings;
-
 let url = 'http://127.0.0.1:8000/bookingapi'
 
-let bookData = []
+var bookData = [];
+const applianceID = document.getElementById("applianceID").innerText;
 
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => bookData.push(data));
+const htmlElement = ''
 
-console.log(bookData)
+async function getData() {
+    try {
+        const rawData = await fetch(url)
+        const data = await rawData.json()
+        return data
+    } catch (err) {
+        console.log('Error in fetching data')
+    }
+}
+
+async function listDates() {
+    const bookingData = await getData()
+        for (let i = 0; i < bookingData.length; i++) {
+            if (bookingData[i]['appliance'] == applianceID) {
+                const paragraph = document.createElement("tr");
+                paragraph.innerHTML = `<tr><th scope="row">Time From: ${bookingData[i]['day_from']} Time To: ${bookingData[i]['day_to']}</th></tr>`;
+                document.getElementById("tableBodyForInsertion").appendChild(paragraph);}
+        }}
+
+getData()
+listDates()
+
